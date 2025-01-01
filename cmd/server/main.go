88131670eeaeedf688de3ab85ca8e432b5c4a8fd
Dylan J. Gerrits <gerritsdylan@gmail.com>
2025-01-01 12:55:49 +0100

    Initialization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	rabbit := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(rabbit)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ. Error:", err)
+		return
+	}
+	defer connection.Close()
+	fmt.Println("The connection to RabbitMQ was successful.")
+
+	// Wait for a signal to exit the application.
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel, os.Interrupt)
+	<-channel
 }
